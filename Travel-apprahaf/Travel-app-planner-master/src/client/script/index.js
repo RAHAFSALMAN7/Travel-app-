bb906a01316c30f84403e4bc74845df1f477e093
Rahafsalman <144127007+RAHAFSALMAN7@users.noreply.github.com>
2024-09-08 15:12:51 +0300

    Add files via upload

---
+++ Travel-apprahaf/Travel-app-planner-master/src/client/script/index.js
@@ -0,0 +1,7 @@
+import { mainFunction } from './js/app';
+import '../styles/style.scss';
+
+// Set up event listeners
+document.addEventListener('DOMContentLoaded', () => {
+    mainFunction();
+});
